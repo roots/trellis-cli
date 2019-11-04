34e86886cc0923ca4cd9fce913bebff59311ed65
TangRufus <tangrufus@gmail.com>
2019-11-04 20:43:54 +0000

    Declare `totalArgs := c.required + c.optional` up front
    https://github.com/roots/trellis-cli/pull/86#discussion_r342258721
    https://github.com/roots/trellis-cli/pull/86#discussion_r342258838

--- cmd/command_argument_validator.go
+++ cmd/command_argument_validator.go
@@ -9,13 +9,14 @@ type CommandArgumentValidator struct {
 
 func (c *CommandArgumentValidator) validate(args []string) (err error) {
 	argCount := len(args)
+	totalArgs := c.required + c.optional
 
 	expectedCount := fmt.Sprintf("exactly %d", c.required)
 	if (c.optional > 0) {
-		expectedCount = fmt.Sprintf("between %d and %d", c.required, c.required+c.optional)
+		expectedCount = fmt.Sprintf("between %d and %d", c.required, totalArgs)
 	}
 
-	if argCount > c.required+c.optional {
+	if argCount > totalArgs {
 		err = fmt.Errorf("Error: too many arguments (expected %s, got %d)\n", expectedCount, len(args))
 	} else if argCount < c.required {
 		err = fmt.Errorf("Error: missing arguments (expected %s, got %d)\n", expectedCount, len(args))
