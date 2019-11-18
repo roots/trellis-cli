99eb736f2da78c2301cbad20ce7f4a58f58bfc1e
TangRufus <tangrufus@gmail.com>
2019-11-18 01:17:27 +0000

    Use `const` instead of `var`

--- cmd/db_opener_sequel_pro.go
+++ cmd/db_opener_sequel_pro.go
@@ -13,7 +13,7 @@ type DBOpenerSequelPro struct {
 	ui cli.Ui
 }
 
-var sequelProSpfTemplate = `
+const sequelProSpfTemplate = `
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
