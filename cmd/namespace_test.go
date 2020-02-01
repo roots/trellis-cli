1da539232cc40283f85a4abf30adf6a275ce6579
TangRufus <tangrufus@gmail.com>
2020-02-01 20:37:54 +0000

    Extract `NamespaceCommand`

---
+++ cmd/namespace_test.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"github.com/mitchellh/cli"
+	"testing"
+)
+
+func TestNamespaceCommandRun(t *testing.T) {
+	expected := cli.RunResultHelp
+	namespaceCommand := &NamespaceCommand{}
+
+	actual := namespaceCommand.Run([]string{})
+
+	if expected != actual {
+		t.Errorf("expected output %d to be %d", actual, expected)
+	}
+}
+
+func TestNamespaceCommandHelp(t *testing.T) {
+	expected := "Help: foo bar"
+	namespaceCommand := &NamespaceCommand{
+		HelpText: expected,
+	}
+
+	actual := namespaceCommand.Help()
+
+	if expected != actual {
+		t.Errorf("expected output %s to be %s", actual, expected)
+	}
+}
+
+func TestNamespaceCommandSynopsis(t *testing.T) {
+	expected := "Synopsis: foo bar"
+	namespaceCommand := &NamespaceCommand{
+		SynopsisText: expected,
+	}
+
+	actual := namespaceCommand.Synopsis()
+
+	if expected != actual {
+		t.Errorf("expected output %s to be %s", actual, expected)
+	}
+}
