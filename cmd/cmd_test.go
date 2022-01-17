c52d50437181f099a3ec8ab34c8f52da68d5275c
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2022-01-16 20:51:18 -0500

    Add command package tests

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -2,9 +2,7 @@ package cmd
 
 import (
 	"fmt"
-	"io"
 	"os"
-	"os/exec"
 	"strings"
 	"testing"
 
@@ -12,32 +10,10 @@ import (
 	"github.com/roots/trellis-cli/command"
 )
 
-func mockExecCommand(stdout io.Writer, stderr io.Writer) func(command string, args []string) *exec.Cmd {
-	return func(command string, args []string) *exec.Cmd {
-		cs := []string{"-test.run=TestHelperProcess", "--", command}
-		cs = append(cs, args...)
-		cmd := exec.Command(os.Args[0], cs...)
-		cmd.Stdout = stdout
-		cmd.Stderr = stderr
-		cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-		return cmd
-	}
-}
-
-func MockExec(t *testing.T, stdout io.Writer, stderr io.Writer) func() {
-	t.Helper()
-
-	command.Mock(mockExecCommand(stdout, stderr))
-
-	return func() {
-		command.Restore()
-	}
-}
-
 func MockUiExec(t *testing.T, ui *cli.MockUi) func() {
 	t.Helper()
 
-	command.Mock(mockExecCommand(ui.OutputWriter, ui.ErrorWriter))
+	command.Mock(command.MockExecCommand(ui.OutputWriter, ui.ErrorWriter))
 
 	return func() {
 		command.Restore()
