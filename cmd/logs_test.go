5cf80c72bc370c2a381a42cff7da5b57d058663b
Ben Word <ben@benword.com>
2025-10-22 12:14:45 -0500

    remove unused logs tests

--- cmd/logs_test.go
+++ cmd/logs_test.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
 	"strings"
 	"testing"
 
@@ -69,99 +66,5 @@ func TestLogsRunValidations(t *testing.T) {
 				t.Errorf("expected output %q to contain %q", combined, tc.out)
 			}
 		})
-	}
-}
-
-func TestLogsRun(t *testing.T) {
-	defer trellis.LoadFixtureProject(t)()
-	trellis := trellis.NewTrellis()
-
-	cases := []struct {
-		name string
-		args []string
-		out  string
-		code int
-	}{
-
-	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			ui := cli.NewMockUi()
-			defer MockUiExec(t, ui)()
-
-			logsCommand := NewLogsCommand(ui, trellis)
-			code := logsCommand.Run(tc.args)
-
-			if code != tc.code {
-				t.Errorf("expected code %d to be %d", code, tc.code)
-			}
-
-			combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
-
-			if !strings.Contains(combined, tc.out) {
-				t.Errorf("expected output %q to contain %q", combined, tc.out)
-			}
-		})
-	}
-}
-
-func TestLogsRunGoAccess(t *testing.T) {
-	defer trellis.LoadFixtureProject(t)()
-	trellis := trellis.NewTrellis()
-
-	tmpDir := t.TempDir()
-
-	// fake goaccess binary to satisfy ok.LookPath
-	goAccessPath := filepath.Join(tmpDir, "goaccess")
-	_, _ = os.OpenFile(goAccessPath, os.O_CREATE, 0555)
-	path := os.Getenv("PATH")
-	t.Setenv("PATH", fmt.Sprintf("PATH=%s:%s", path, tmpDir))
-
-	cases := []struct {
-		name string
-		args []string
-		out  []string
-		code int
-	}{
-
-	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			ui := cli.NewMockUi()
-			defer MockUiExec(t, ui)()
-
-			logsCommand := NewLogsCommand(ui, trellis)
-			code := logsCommand.Run(tc.args)
-
-			if code != tc.code {
-				t.Errorf("%s - actual code %d expected to be %d", tc.name, code, tc.code)
-			}
-
-			combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
-
-			for _, out := range tc.out {
-				if !strings.Contains(combined, out) {
-					t.Errorf("%s - actual output %q expected to contain %q", tc.name, combined, out)
-				}
-			}
-		})
-	}
-}
-
-func TestLogsHelperProcess(t *testing.T) {
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
-	}
-
-	switch os.Args[3] {
-	case "ssh":
-		fmt.Fprintf(os.Stdout, "nginx log")
-		os.Exit(0)
-	case "tail":
-		os.Exit(0)
-	default:
-		t.Fatalf("unexpected command %s", os.Args[3])
 	}
 }
